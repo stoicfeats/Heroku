d31ccc249eff0efacca599137432e7933060f1ec
Yash Chaudhary <chaudharyyash088@gmail.com>
2020-10-02 22:31:13 +0530

    updates

--- Salary-Prediction/model.py
+++ Salary-Prediction/model.py
@@ -37,4 +37,4 @@ pickle.dump(regressor, open('model.pkl','wb'))
 
 # Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[5, 6, 7]]))
+print(model.predict([[5, 6, 7]])) 
