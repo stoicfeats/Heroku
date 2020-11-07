d48413f676c417c0836d448c80fe63a55804b92e
Yash Chaudhary <chaudharyyash088@gmail.com>
2020-11-07 22:36:55 +0530

    beautify

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ def predict():
     output = round(prediction[0], 2)
 
     if output == 1 :
-        return render_template('index.html', prediction_text="Positive")
+        return render_template('index.html', prediction_text="You have diabetes, please contact a doc ASAP.")
     else :
-        return render_template('index.html', prediction_text="Negative")
+        return render_template('index.html', prediction_text="You do not have diabetes, congrats :')")
 
 if __name__ == "__main__":
     app.run(debug=True)
