a26194f69786f7b75635642b74d813d2470a6c61
Yash Chaudhary <chaudharyyash088@gmail.com>
2020-11-07 22:24:49 +0530

    changed the spelling

---
+++ app.py
@@ -0,0 +1,36 @@
+import numpy as np 
+from flask import Flask, request, render_template
+import pickle 
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+#Define home route
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+#Define diagnosis route
+@app.route("/predict", methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI 
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    if output == 1 :
+        return render_template('index.html', prediction_text="Positive")
+    else :
+        return render_template('index.html', prediction_text="Negative")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
+    
+    
+    
